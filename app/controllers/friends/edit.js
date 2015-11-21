3a15fbe77b1011f9eb230de0c34fc37a7b0e52c9
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2015-11-21 10:03:59 -0500

    Adding many more friends routes and controllers.

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,7 +3,7 @@ import FriendBaseControler from './base';
 export default FriendBaseControler.extend({
   actions: {
     cancel() {
-      this.transitionToRoute('friends');
+      this.transitionToRoute('friends.show', this.get('model'));
       return false;
     }
   }
--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,13 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('friend');
-  },
   actions: {
     delete(friend) {
-      friend.destroyRecord();
-      return false;
+      friend.destroyRecord().then(() => {
+          this.transitionTo('friends.index');
+      });
     }
   }
 });
