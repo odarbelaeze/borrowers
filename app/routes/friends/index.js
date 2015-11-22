4d47e32e266beb2dd4a0c5c9075968327d02a0a3
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2015-11-21 20:38:05 -0500

    Refactoring delete action.

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -4,10 +4,4 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
   },
-  actions: {
-    delete(friend) {
-      friend.destroyRecord();
-      return false;
-    }
-  }
 });
--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,11 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  actions: {
-    delete(friend) {
-      friend.destroyRecord().then(() => {
-          this.transitionTo('friends.index');
-      });
-    }
-  }
-});
+export default Ember.Route.extend({ });
